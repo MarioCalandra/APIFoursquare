f7374eda7fb4f3ede6c5011695fd781f6086eb28
MarioCalandra <mario.calandra@outlook.com>
2018-01-29 12:06:52 +0100

    Update search_cibi.py

--- dati/search_cibi.py
+++ dati/search_cibi.py
@@ -2,8 +2,8 @@ import json, requests
 from jsonFSmng import json_search
 from charts import ColumnChart, PieChart
 
-client_id = 'HY5VTLYMVXHH0SOTC2MR2PGZTIGZJ2ZQN4T4RHDADQNWQWLL'
-client_secret = 'RLZUPWGYNKKLJN13WUNGFTI4SQJKWQFHRPCZ1RVYID0I2BBO'
+client_id = 'SMM'
+client_secret = 'SMM'
 versioning = '20180122'
 
 category = '4d4b7105d754a06374d81259' #categoria cibi
